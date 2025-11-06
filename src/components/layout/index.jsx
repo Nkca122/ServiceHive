5b21b4ec853ab46ef89b7d39c021eb8bd70ab302
Nikunj Chauhan <nkca122@gmail.com>
2025-11-06 23:40:48 +0530

    layout styling break fix

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 export default function Layout(){
     return (
         <>
-            <main className="bg-secondary w-screen min-h-screen">
+            <main className="bg-secondary w-screen h-screen min-h-screen">
                 <Outlet />
             </main>
         </>
